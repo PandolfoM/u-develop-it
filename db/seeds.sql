51f7edbd491fcdc623132dd8add07c633b86ad7d
Matthew Pandolfo <matt@pandolfo.com>
2021-10-08 16:17:56 -0400

    create database with table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ahsoka', 'Tano', 1),
+  ('Anakin', 'Skywalker', 1),
+  ('Obi-Wan', 'Kenobi', 1),
+  ('Darth', 'Maul', 0),
+  ('Count', 'Dooku', 0),
+  ('Emperor', 'Palpatine', 0);
