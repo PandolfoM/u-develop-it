e7149fe36d2337bbeea1d5efe89dd598f2152376
Matthew Pandolfo <matt@pandolfo.com>
2021-10-08 16:52:05 -0400

    database calls

--- server.js
+++ server.js
@@ -16,9 +16,37 @@ const db = mysql.createConnection(
   console.log("Connected to the election database")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// Create candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+              VALUES (?,?,?,?)`;
+const params = [1, "Ahsoka", "Tano", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
+
+// Delete candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
