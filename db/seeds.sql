c4184533665b4abf0caccc50c413ca4f4e8cf7a7
Matthew Pandolfo <matt@pandolfo.com>
2021-10-10 10:34:10 -0400

    party_name to each candidate

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,14 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
+INSERT INTO parties (name, description)
+VALUES  
+  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
+  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
+  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
 VALUES
-  ('Ahsoka', 'Tano', 1),
-  ('Anakin', 'Skywalker', 1),
-  ('Obi-Wan', 'Kenobi', 1),
-  ('Darth', 'Maul', 0),
-  ('Count', 'Dooku', 0),
-  ('Emperor', 'Palpatine', 0);
+  ('Ahsoka', 'Tano', 1, 1),
+  ('Anakin', 'Skywalker', 1, 1),
+  ('Obi-Wan', 'Kenobi', 1, 1),
+  ('Darth', 'Maul', 2, 0),
+  ('Count', 'Dooku', 2, 0),
+  ('Emperor', 'Palpatine', 2, 0);
