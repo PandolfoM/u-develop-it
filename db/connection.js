a125cc043998ce2c356ae415b978a14c0b99fa20
Matthew Pandolfo <matt@pandolfo.com>
2021-10-16 12:44:58 -0400

    password

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    password: "Pandolfo2",
+    password: "123",
     database: "election",
   },
   console.log("Connected to the election database")
